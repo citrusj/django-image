e1bed6ff7edebb3df579a13d47d94aeebfbea28b
citrusj <citrusj0123@gmail.com>
2020-05-18 22:56:26 +0900

    add image upload

--- basicsite/urls.py
+++ basicsite/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 import board.views
 
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',board.views.home, name="home"),
@@ -25,3 +27,4 @@ urlpatterns = [
     path('edit/<int:pk>', board.views.edit, name="edit"),
     path('delete/<int:pk>', board.views.delete, name="delete"),
 ]
+urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- board/models.py
+++ board/models.py
@@ -7,3 +7,4 @@ class Content(models.Model):
     title=models.CharField(max_length=200)
     pub_date = models.DateTimeField(default=timezone.now)
     body = models.TextField(default='')
+    image = models.ImageField(upload_to='images/',blank=True)
