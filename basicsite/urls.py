650f4117ba84ef25bf798ef30e2762d8dbb79680
citrusj <citrusj0123@gmail.com>
2020-05-20 20:00:20 +0900

    Add file upload

--- basicsite/urls.py
+++ basicsite/urls.py
@@ -19,6 +19,7 @@ import board.views
 
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',board.views.home, name="home"),
--- board/models.py
+++ board/models.py
@@ -7,4 +7,8 @@ class Content(models.Model):
     title=models.CharField(max_length=200)
     pub_date = models.DateTimeField(default=timezone.now)
     body = models.TextField(default='')
+
     image = models.ImageField(upload_to='images/',blank=True)
+
+
+    file = models.FileField(upload_to='documents/%Y.%m',blank=True) #파일 저장 위치가 document/(년.월)폴더 로 설정됨.
